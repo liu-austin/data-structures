01cae4e756a9825c947ebe5c0c81079548529a04
liu-austin <austinliu279@gmail.com>
2019-10-24 16:31:37 -0700

    adds the LinkedList and Tree datastructures

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,41 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (!list.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail = this.head;
+      while (this.tail.next) {
+        this.tail = this.tail.next;
+      }
+      this.tail.next = newNode;
+      this.tail = this.tail.next;
+    }
   };
 
   list.removeHead = function() {
+    if (this.head) {
+      var returnValue = this.head.value;
+      this.head = this.head.next;
+      return returnValue;
+    }
   };
 
   list.contains = function(target) {
+    var current;
+    if (this.head) {
+      current = this.head;
+      while (current) {
+        if (current.value === target) {
+          return true;
+        } else {
+          current = current.next;
+        }
+      }
+    }
+    return false;
   };
 
   return list;
@@ -27,3 +56,6 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+// list.contains: O(n),
+// list.removeHead: O(1),
+// list.addToTail: O(n)
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,23 +1,37 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
+  newTree.childKey = 0;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = {};
+
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
 
 var treeMethods = {};
-
 treeMethods.addChild = function(value) {
+  var childTree = Tree(value);
+  this.children[this.childKey] = childTree;
+  this.childKey += 1;
 };
 
 treeMethods.contains = function(target) {
+  var contains = 0;
+  if (this.value === target) {
+    contains += 1;
+  }
+  for (var i in this.children) {
+    contains += this.children[i].contains(target);
+  }
+  return contains ? true : false;
 };
-
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+// treeMethods.addChild: O(1),
+// treeMethods.contains: O(n)
